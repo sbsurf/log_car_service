b0bc2b063539be97e6febc433a0ada9162b0aa56
yuri <yuri@nanakdev.com>
2013-05-31 13:19:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogCarService::Application.config.secret_key_base = '32d3f5daec36d130871ce7ac41239483eb1f0adcca11225b25f83c73295baf47ad58c3729ab696337945432ba389fc1a203fb574f5dd9f0aca6fac7de0677c0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LogCarService::Application.config.session_store :cookie_store, key: '_log_car_service_session'
